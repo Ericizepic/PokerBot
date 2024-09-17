30685809717ab2d5f8730bf62b626fdddaf531b3
Eric Du <ericdu5000@gmail.com>
2024-09-16 21:53:17 -0400

    updated readme

---
+++ src/deck.cc
@@ -0,0 +1,27 @@
+#include "deck.h"
+#include "shared.h"
+
+Deck::Deck() {
+    cards = defaultDeckCards;
+
+    std::random_shuffle(cards.begin(), cards.end());
+
+    playerCards = std::vector<std::vector<int>>(2, std::vector<int>(0));
+    playerCards[0].push_back(cards[0]);
+    playerCards[0].push_back(cards[1]);
+    playerCards[1].push_back(cards[2]);
+    playerCards[1].push_back(cards[3]);
+}
+
+std::vector<int> Deck::getPlayerCards(int player){
+    return playerCards[player];
+}
+
+
+std::vector<int> Deck::getCommunityCards(int round_num){
+    std::vector<int> communityCards;
+    for (int i = 0; i<round_num; i++)
+        communityCards.push_back(cards[4+i]);
+
+    return communityCards;
+}
